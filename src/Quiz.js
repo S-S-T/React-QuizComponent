39ce121d0056a968c23baa649896f835f6db9099
Sanford Schaffer <sschaffer.ctr@kr.af.mil>
2020-04-14 20:02:12 -0400

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react'
+import './App.css'
+import Quiz from './Quiz.js'
+import { render } from 'enzyme'
 
 let quizData = require('./quiz_data.json')
+class Quiz extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { quiz_data: 1 }
+
+    }
+}
+
+
+
+render() {
+    return (
+        <div>QUIZ</div>   
+    );
+}
+
 
 
 
