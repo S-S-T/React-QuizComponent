00814ebc2b4c1d8a971e4dff88b568994f94ffc7
Sanford Schaffer <sschaffer.ctr@kr.af.mil>
2020-04-14 20:23:22 -0400

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,14 +8,13 @@ let quizData = require('./quiz_data.json')
 class Quiz extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { quiz_position: 1 }
-
-    
+        this.state = { quiz_position: 1 };
+    }
     render() {
         return (
         <div><div className="QuizQuestion">{quizData.quiz_quesiton[0].instruction_text}</div></div>   
         )
-        }
     }
+    
 }
 export default Quiz
